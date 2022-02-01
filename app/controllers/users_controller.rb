98ddd3dd6c5558c7b14f2c122df0f6f55528b6ac
Name <toshiki21320@icloud.com>
2022-02-01 16:53:02 +0000

    プロフ画像編集まで

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,48 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(
+    name: params[:name], 
+    email: params[:email],
+    image: "default.png",
+    )
+    if @user.save
+      flash[:notice] = 'ユーザーを新規登録しました！'
+      redirect_to user_url @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+   @user = User.find(params[:id])
+   @user.name = params[:name]
+   @user.email = params[:email]
+   if params[:image]
+     @user.image="user_#{@user.id}.png"
+     File.binwrite("public/user_images/#{@user.image}",params[:image].read)
+   end
+   if @user.save
+     flash[:notice] = "ユーザー情報を編集しました。"
+     redirect_to user_url @user
+   else
+    render :edit
+   end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 投稿内容を１行ずつ記述して配列にまとめます。
+posts = [
+  '今日からRailsの学習を始めます。',
+  '毎日Rails学習中！プログラミング楽しい！',
+  'だんだん内容が難しくなってきたぞ・・・。',
+  'レッスンで作っているアプリが形になってきたな〜、この調子だ！',
+  'どこでもプログラミングできるように、ノートパソコン買っちゃおうかな。',
+  'バナナ食べて勉強ガンバロウ',
+  'CSSでデザインいじるの楽しいな〜',
+  'カフェでプログラミングすると捗る〜',
+  'この前買ったパソコンいい感じだ。',
+  '友達もフリーエンジニアに興味があるみたい！',
+  '干し草食べて勉強頑張ろう',
+  'ついに私も月収５０万円に・・・！',
+  '地道に続ければ、しっかり身につくことがわかった。',
+]
+
+# 投稿内容の配列を繰り返し処理して投稿データを作成します。
+posts.each { |post| Post.create!(content: post) }
+
+# ユーザーをひとりずつ作成します。
+User.create!(name: 'フリーエンジニア君', email: 'engineer@email.com', image: 'user_1.png')
+User.create!(name: 'フリーデザイナーさん', email: 'designer@email.com', image: 'user_2.png')
+User.create!(name: 'アルパカさん', email: 'alpaca@email.com', image: 'user_3.png')
+User.create!(name: 'ゴリラくん', email: 'gorilla@email.com', image: 'user_4.png')
+User.create!(name: 'セレブエンジニアさん', email: 'celeb@email.com', image: 'user_5.png')
+
+
